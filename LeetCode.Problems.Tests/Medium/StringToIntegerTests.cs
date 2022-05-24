590af575b6120a08650be2a3043e79e217b8ebaf
Alexey Chuvagin <alexey.chuvagin@outlook.com>
2022-05-24 21:34:19 +0300

    Refactor StringToInteger

---
+++ LeetCode.Problems.Tests/Medium/StringToIntegerTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using static LeetCode.Problems.Medium.StringToInteger;
+
+namespace LeetCode.Problems.Tests.Medium;
+
+public sealed class StringToIntegerTests
+{
+    [Theory]
+    [InlineData("   -42", -42)]
+    [InlineData("-42   ", -42)]
+    [InlineData("4193 with words", 4193)]
+    [InlineData("words and 987", 0)]
+    [InlineData("-91283472332", -2147483648)]
+    [InlineData("-2147483648", -2147483648)]
+    public void Test(string input, int output) 
+        => Assert.Equal(output, MyAtoi(input));
+}
