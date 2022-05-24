8c54764c252115dde3f45308f4b79e84b74e6d7a
Alexey Chuvagin <alexey.chuvagin@outlook.com>
2022-05-24 21:09:08 +0300

    Refactor LongestPalindromicSubstring

---
+++ LeetCode.Problems.Tests/Medium/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using static LeetCode.Problems.Medium.LongestPalindromicSubstring;
+
+namespace LeetCode.Problems.Tests.Medium;
+
+public sealed class LongestPalindromicSubstringTests
+{
+    [Theory]
+    [InlineData("babad", "bab")]
+    [InlineData("cbbd", "bb")]
+    [InlineData("a", "a")]
+    [InlineData("bb", "bb")]
+    [InlineData("ccd", "cc")]
+    [InlineData("aaaa", "aaaa")]
+    public void Test(string input, string output) 
+        => Assert.Equal(output, Solution(input));
+}
---
+++ LeetCode.Problems/Medium/LongestPalindromicSubstring.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Problems.Medium;
+
+/// <summary>
+/// 5. Given a string s, find the longest palindromic substring in s. 
+/// You may assume that the maximum length of s is 1000.
+/// <remarks>Medium</remarks>
+/// <seealso cref="https://leetcode.com/problems/longest-palindromic-substring/"/>
+public static class LongestPalindromicSubstring
+{
+    public static string Solution(string s)
+    {
+        var result = string.Empty;
+
+        for (var pos = 0; pos < s.Length; pos++)
+        {
+            int even_left = pos;
+            int odd_left = pos - 1;
+            int left = pos;
+            int right = pos + 1;
+
+            while (right < s.Length)
+            {
+                var isEqual = false;
+
+                if (even_left >= 0 && s[even_left].Equals(s[right]))
+                {
+                    isEqual = true;
+                    left = even_left;
+                    even_left--;
+                }
+                else even_left = -1;
+
+                if (odd_left >= 0 && s[odd_left].Equals(s[right]))
+                {
+                    isEqual = true;
+                    left = odd_left;
+                    odd_left--;
+                }
+                else odd_left = -1;
+
+                if (!isEqual) break;
+
+                right++;
+            }
+
+            if (right - left > result.Length)
+                result = s[left..right];
+        }
+
+        return result;
+    }
+}
