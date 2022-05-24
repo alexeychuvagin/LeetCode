d1409be8cfe312d1af3bd60e18a25d700b222cbd
Alexey Chuvagin <alexey.chuvagin@outlook.com>
2022-05-24 21:25:03 +0300

    Refactor ReverseInteger

---
+++ LeetCode.Problems.Tests/Easy/ReverseIntegerTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using static LeetCode.Problems.Easy.ReverseInteger;
+
+namespace LeetCode.Problems.Tests.Easy;
+
+public sealed class ReverseIntegerTests
+{
+    [Theory]
+    [InlineData(123, 321)]
+    [InlineData(-123, -321)]
+    [InlineData(120, 21)]
+    [InlineData(0, 0)]
+    [InlineData(1534236469, 0)]
+    public void Test(int input, int output) 
+        => Assert.Equal(output, Reverse(input));
+}
---
+++ LeetCode.Problems/Easy/ReverseInteger.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Problems.Easy;
+
+/// <summary>
+/// 7. Reverse Integer
+/// Given a 32-bit signed integer, reverse digits of an integer.
+/// Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: 
+/// [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+/// </summary>
+/// <remarks>Easy</remarks>
+/// <seealso cref="https://leetcode.com/problems/reverse-integer/"/>
+public static class ReverseInteger
+{
+    public static int Reverse(int x)
+    {
+        var result = 0;
+
+        while (x != 0)
+        {
+            var pop = x % 10;
+            x /= 10;
+
+            if (result > int.MaxValue / 10 || result == int.MaxValue / 10 && pop > 7) return 0;
+            if (result < int.MinValue / 10 || result == int.MinValue / 10 && pop < -8) return 0;
+
+            result = result * 10 + pop;
+        }
+
+        return result;
+    }
+}
